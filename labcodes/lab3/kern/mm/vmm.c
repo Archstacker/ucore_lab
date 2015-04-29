5b85ed056ea674feddaac2b7e9f054eda04a434e
ArchStacker <ArchStacker@gmail.com>
2015-04-29 22:54:09 +0800

    Finish exercise1 in lab3 : do_pgfault

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -395,8 +395,13 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
             goto failed;
         }
    }
+    
 #endif
-   ret = 0;
+    ptep = get_pte(mm->pgdir, addr, 1);
+    if(*ptep == 0) {
+        pgdir_alloc_page(mm->pgdir, addr, perm);
+    }
+    ret = 0;
 failed:
     return ret;
 }
