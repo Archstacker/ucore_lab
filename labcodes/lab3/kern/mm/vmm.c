c360f351bfd203b8d800ce512ee6c82dffd69723
ArchStacker <ArchStacker@gmail.com>
2015-05-07 18:16:15 +0800

    Finish the clock page-replacement algorithm

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -405,8 +405,14 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
         if(swap_init_ok) {
             struct Page  *page = NULL;
             swap_in(mm, addr, &page);
-            page_insert(mm->pgdir, page, addr, perm);
+            if (page != NULL) {
+                if (page_insert(mm->pgdir, page, addr, perm) != 0) {
+                    free_page(page);
+                    goto failed;
+                }
+            }
             swap_map_swappable(mm, addr, page, 1);
+            page->pra_vaddr = addr;
         }
         else {
             cprintf("no swap_init_ok but petp is %x, failed\n", &ptep);
