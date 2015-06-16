7253ba692b4e44781414dfa0fb5dab38756841c3
ArchStacker <ArchStacker@gmail.com>
2015-06-16 21:57:40 +0800

    Add proc_tick to stride_scheduler in lab7.Use run_timer_list instead of sched_class_proc_tick

--- labcodes/lab7/kern/schedule/default_sched_stride.c
+++ labcodes/lab7/kern/schedule/default_sched_stride.c
@@ -138,6 +138,12 @@ stride_pick_next(struct run_queue *rq) {
 static void
 stride_proc_tick(struct run_queue *rq, struct proc_struct *proc) {
      /* LAB6: YOUR CODE */
+    if (proc->time_slice > 0) {
+        proc->time_slice --;
+    }
+    if (proc->time_slice == 0) {
+        proc->need_resched = 1;
+    }
 }
 
 struct sched_class default_sched_stride_class = {
--- labcodes/lab7/kern/trap/trap.c
+++ labcodes/lab7/kern/trap/trap.c
@@ -244,10 +244,7 @@ trap_dispatch(struct trapframe *tf) {
 	     * run_timer_list
          */
         ticks++;
-        if(ticks % TICK_NUM == 0){
-            print_ticks();
-            sched_class_proc_tick(current);
-        }
+        run_timer_list();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
